8da12c0186c94613b5261d8b69ea244a0e1e5486
maritaria <bramkamies@gmail.com>
2018-03-14 17:54:20 +0100

    Formatting + getPlayerStatus

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ const express_handlebars = require('express-handlebars');
 const app = express();
 const Promise = require('promise');
 
-app.engine('handlebars', express_handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', express_handlebars({
+	defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 app.get('/player/:id', function(request, response) {
 	Promise.all([
-		paladins.getPlayer(request.params.id),
-		paladins.getChampionRanks(request.params.id),
-		paladins.getPlayerLoadouts(request.params.id),
-	])
-		.then((results)=>{
-			console.log(results);
+			paladins.getPlayer(request.params.id),
+			paladins.getChampionRanks(request.params.id),
+			paladins.getPlayerLoadouts(request.params.id),
+		])
+		.then((results) => {
 			response.render('player', {
 				player: results[0],
 				champions: results[1],
 			});
 		})
-		.catch((error)=>{
+		.catch((error) => {
 			response.render('error', {
 				error: {
 					title: 'Failed to get player data',
@@ -30,11 +31,11 @@ app.get('/player/:id', function(request, response) {
 		});
 });
 
-app.get('/', function (request, response) {
+app.get('/', function(request, response) {
 	response.render('home');
 });
 
-paladins.init(process.env.PALADINS_API_DEV, process.env.PALADINS_API_KEY, (err)=>{
+paladins.init(process.env.PALADINS_API_DEV, process.env.PALADINS_API_KEY, (err) => {
 	if (err) throw err;
 	app.listen(3000, () => console.log('Running on port 3000'));
 });
